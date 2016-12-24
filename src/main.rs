9caf19b1339c54194431008e15a4867e443c7d65
Max Holder <mxhold@gmail.com>
2016-12-24 07:45:50 -0800

    Extract database_connection

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,19 @@ extern crate router;
 extern crate r2d2;
 extern crate r2d2_sqlite;
 extern crate rusqlite;
-extern crate uuid;
 
 use iron::prelude::*;
 use iron::status;
 use std::io::Read;
 use r2d2_sqlite::SqliteConnectionManager;
 
-type SqlitePool = r2d2::Pool<SqliteConnectionManager>;
-type SqlitePooledConnection = r2d2::PooledConnection<SqliteConnectionManager>;
+mod database_connection;
+use database_connection::DatabaseConnection;
 
-struct ConnectionPool;
+pub type SqlitePool = r2d2::Pool<SqliteConnectionManager>;
+pub type SqlitePooledConnection = r2d2::PooledConnection<SqliteConnectionManager>;
+
+pub struct ConnectionPool;
 impl iron::typemap::Key for ConnectionPool {
     type Value = SqlitePool;
 }
@@ -25,44 +27,29 @@ fn setup_connection_pool() -> SqlitePool {
     r2d2::Pool::new(config, manager).unwrap()
 }
 
-fn get_connection(req: &mut Request) -> SqlitePooledConnection {
-    let pool = req.get::<persistent::Read<ConnectionPool>>().unwrap();
-    pool.get().unwrap()
-}
-
 fn post_pastebin(req: &mut Request) -> IronResult<Response> {
-    let conn = get_connection(req);
+    let conn = DatabaseConnection::new(req);
 
     let mut req_body = String::new();
     req.body.read_to_string(&mut req_body).unwrap();
 
-    match insert_paste(&conn, &req_body) {
+    match conn.insert_paste(&req_body) {
         Ok(id) => Ok(Response::with((status::Ok, format!("{}{}\n", req.url, id)))),
         Err(_) => Ok(Response::with((status::ServiceUnavailable, ""))),
     }
 }
 
 fn get_pastebin(req: &mut Request) -> IronResult<Response> {
-    let conn = get_connection(req);
+    let conn = DatabaseConnection::new(req);
 
     let id = req.extensions.get::<router::Router>().unwrap().find("id").unwrap();
 
-    match get_paste_body_by_id(&conn, &id) {
+    match conn.get_paste_body_by_id(&id) {
         Some(body) => {
             Ok(Response::with((status::Ok, body)))
         }
         None => Ok(Response::with((status::NotFound, ""))),
     }
-}
-
-fn get_paste_body_by_id(conn: &SqlitePooledConnection, id: &str) -> Option<String> {
-    let query = "SELECT body FROM pastes WHERE id = $1";
-    conn.query_row(query, &[&id], |row| row.get(0)).ok()
-}
-
-fn insert_paste(conn: &SqlitePooledConnection, body: &str) -> Result<String, rusqlite::Error> {
-    let id = uuid::Uuid::new_v4().to_string();
-    conn.execute("INSERT INTO pastes VALUES ($1, $2)", &[&id, &body]).and(Ok(id))
 }
 
 fn setup_database(conn: &SqlitePooledConnection) {
